efa4d2e712636b147e597ee6241eebda9af54cad
Arhi74 <alexey.andryushin@mail.ru>
2023-11-04 15:01:31 +0500

    Добавлен обработчик команд /start / help

--- app.py
+++ app.py
@@ -3,10 +3,15 @@ from bot_token import BotToken
 
 bot = telebot.TeleBot(BotToken)
 
+keys = {'доллар': 'USD'}
 
-@bot.message_handler()
-def echo_test(message: telebot.types.Message):
-    bot.send_message(message.chat.id, 'hello!')
+
+@bot.message_handler(commands=['start', 'help'])
+def start_help(message: telebot.types.Message):
+    text = ('Чтобы начать работу с ботом введите команду в следующем формате:\n <имя валюты> <имя валюты, \
+в какую надо перевести> <количество переводимой валюты>\nНапример: рубль доллар 100\
+\nДля вывода списка валют: /values')
+    bot.reply_to(message, text)
 
 
 bot.polling()
