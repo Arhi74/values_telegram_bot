0345744dea40ce35c847be0a52cd335f689fa199
Arhi74 <alexey.andryushin@mail.ru>
2023-11-04 16:32:59 +0500

    Добавлен обработчик команд /values и начата работа над обработчиком convert

--- app.py
+++ app.py
@@ -8,10 +8,23 @@ keys = {'доллар': 'USD'}
 
 @bot.message_handler(commands=['start', 'help'])
 def start_help(message: telebot.types.Message):
-    text = ('Чтобы начать работу с ботом введите команду в следующем формате:\n <имя валюты> <имя валюты, \
-в какую надо перевести> <количество переводимой валюты>\nНапример: рубль доллар 100\
-\nДля вывода списка валют: /values')
+    text = 'Чтобы начать работу с ботом введите команду в следующем формате:\n <имя валюты> <имя валюты, \
+в какую надо перевести> <количество>\nНапример: рубль доллар 100\
+\nДля вывода списка валют: /values'
     bot.reply_to(message, text)
+
+
+@bot.message_handler(commands=['values'])
+def values(message: telebot.types.Message):
+    text = 'Доступные валюты:'
+    for key in keys.keys():
+        text = '\n'.join((text, key))
+    bot.reply_to(message, text)
+
+
+@bot.message_handler(content_types=['text'])
+def convert(message: telebot.types.Message):
+    quote, base, amount = message.text.split()
 
 
 bot.polling()
